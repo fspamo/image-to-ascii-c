d13aaeee91b487e5017aa3b7f87b15a818565492
Cahit Atilla Bozkurt <cbozkurt@k2m08s03.42kocaeli.com.tr>
2026-02-20 17:02:43 +0300

    norm

--- main.c
+++ main.c
@@ -4,7 +4,20 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned char scale[256] = {
+	int width;
+	int height;
+	int channels;
+
+	int brightness;
+
+	unsigned char	*scale;
+	unsigned char	*img;
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+
+	scale = 
+		[
 		'$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a', 'h', 'k', 'b', 'd',
 		'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X', 'z', 'c',
 		'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', '/', '\\', '|', '(', ')', '1', '{', '}',
@@ -26,26 +39,24 @@ int main(int argc, char *argv[])
 		' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
 		' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
 		' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '
-	};
+			];
 	if (argc == 1)
 		return 0;
-	int width, height, channels;
-
-	unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 0);
+	img = stbi_load(argv[1], &width, &height, &channels, 0);
 
 	if (img == NULL) {
 		printf("img fail\n");
 		return 1;
 	}
 
-	for (int y = 0; y < height; y++) {
+	for (int y = 0; y < height; y+=4) {
 		for (int x = 0; x < width; x++) {
 			int index = (y * width + x) * channels;
-			unsigned char r = img[index];
-			unsigned char g = img[index + 1];
-			unsigned char b = img[index + 2];
+			r = img[index];
+			g = img[index + 1];
+			b = img[index + 2];
 			// unsigned char a = (channels == 4) ? img[index + 3] : 255;
-			int brightness = 0.299f * r + 0.587f * g + 0.114f * b;
+			brightness = 0.299f * r + 0.587f * g + 0.114f * b;
 
 			if (x == 0)
 				printf("\n");
@@ -54,10 +65,9 @@ int main(int argc, char *argv[])
 		}
 	}
 
-
-	printf("Width: %d\n", width);
-	printf("Height: %d\n", height);
-	printf("Channels: %d\n", channels);
+	// printf("Width: %d\n", width);
+	// printf("Height: %d\n", height);
+	// printf("Channels: %d\n", channels);
 
 	stbi_image_free(img);
 
